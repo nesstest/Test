8452b087edddfc3484c7aa3aecbb6a8f1d979053
Steve Developer <steven.thorne@ons.gov.uk>
2015-06-25 12:35:54 +0100

    Stuff

--- src/com/ness/test/Main.java
+++ src/com/ness/test/Main.java
@@ -11,6 +11,24 @@ import org.glassfish.embeddable.archive.ScatteredArchive;
 public class Main {
 	
 	public static void main(String[] args) throws Exception {
+		
+        String port = System.getenv("PORT");
+        port = port != null ? port : "8080";
+        GlassFishProperties gfProps = new GlassFishProperties();
+        gfProps.setPort("http-listener", Integer.parseInt(port));
+
+        GlassFish glassfish = GlassFishRuntime.bootstrap().newGlassFish(gfProps);
+        glassfish.start();
+
+        File webRoot = new File("WebContent/");
+        File classRoot = new File("target", "classes");
+
+        Deployer deployer = glassfish.getDeployer();
+        ScatteredArchive archive = new ScatteredArchive("hello", ScatteredArchive.Type.WAR, webRoot);
+        archive.addClassPath(classRoot);
+        deployer.deploy(archive.toURI());
+		
+		
 //		String webappDirLocation = "WebContent/";
 //
 //		// The port that we should run on can be set into an environment
